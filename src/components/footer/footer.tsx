b9bb5dae27cc42ad85fecab83150befc642d985e
koudaihirata <2230051@ecc.ac.jp>
2024-03-02 01:52:36 +0900

    ヘッダーとフッター完成

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Footer() {
+    return (
+      <footer style={{width:'100%',height:'164px',display:'flex',alignItems:'center',justifyContent:'center',background:'#D9D9D9'}}>
+        <small>
+            <p style={{fontSize:'18px'}}>&copy; 2024 Hirata&apos;s portfolio</p>
+        </small>
+      </footer>
+    );
+  }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+export default function Header() {
+    return (
+        <header style={{width:'100%',height:'74px',display:'flex',alignItems:'center',justifyContent:'space-between'}}>
+            <h1 style={{marginLeft:'30px'}}><Link href="/"><Image src="/logo.svg" alt="logo" width={104} height={56}/></Link></h1>
+            <nav style={{marginRight:'100px'}}>
+                <ul style={{display:'flex',gap:'50px'}}>
+                    <li style={{listStyle:'none'}}><Link href="#" style={{textDecoration:'none',color:'#9F9F9F',fontSize:'24px'}}>profile</Link></li>
+                    <li style={{listStyle:'none',}}><Link href="#" style={{textDecoration:'none',color:'#9F9F9F',fontSize:'24px'}}>project</Link></li>
+                    <li style={{listStyle:'none',}}><Link href="#" style={{textDecoration:'none',color:'#9F9F9F',fontSize:'24px'}}>contact</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
