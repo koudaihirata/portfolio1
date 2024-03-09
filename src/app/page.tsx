f62736a5a4e16e58656b03b3a78dee2ba5de4e79
koudaihirata <2230051@ecc.ac.jp>
2024-03-09 23:55:37 +0900

    ファーストビュー（仮）完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-    
+      <section className="homeMein">
+        <h2><Image src="/logo.svg" alt="logo" width={335} height={182}/></h2>
+      </section>
     </main>
   );
 }
