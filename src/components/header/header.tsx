3c2da926e69620993e693fe57413f64302121089
koudaihirata <2230051@ecc.ac.jp>
2024-03-19 23:36:58 +0900

    ヘッダー完成

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,19 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
-
+import "@/components/header/header.scss";
 
 
 export default function Header() {
     return (
-        <header style={{width:'100%',height:'74px',display:'flex',alignItems:'center',justifyContent:'space-between',position:'fixed',top:'0',left:'0',zIndex:'1000',background:'rgba(255, 255, 255, .5)'}}>
+        <header style={{width:'100%',height:'74px',display:'flex',alignItems:'center',justifyContent:'space-between',position:'fixed',top:'0',left:'0',zIndex:'1000',background:'rgba(255, 255, 255, .5)',borderBottom:'1px solid rgba(255, 255, 255, .5)'}}>
             <h1 style={{marginLeft:'30px'}}><Link href="/"><Image src="/logo.svg" alt="logo" width={104} height={56}/></Link></h1>
             <nav style={{marginRight:'100px'}}>
-                <ul style={{display:'flex',gap:'50px'}}>
-                    <li style={{listStyle:'none'}}><Link href="/Profile" style={{textDecoration:'none',color:'#9F9F9F',fontSize:'24px'}}>profile</Link></li>
-                    <li style={{listStyle:'none',}}><Link href="#" style={{textDecoration:'none',color:'#9F9F9F',fontSize:'24px'}}>project</Link></li>
-                    <li style={{listStyle:'none',}}><Link href="#" style={{textDecoration:'none',color:'#9F9F9F',fontSize:'24px'}}>contact</Link></li>
+                <ul style={{display:'flex',gap:'20px'}}>
+                    <li className="listStyle"><Link href="/Profile" className="HLink">profile</Link></li>
+                    <li className="listStyle"><Link href="#" className="HLink">project</Link></li>
+                    <li className="listStyle"><Link href="#" className="HLink">contact</Link></li>
                 </ul>
             </nav>
         </header>
