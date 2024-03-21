e74245d03721fafddf4401422caa5bd7e0c0a2fb
koudaihirata <2230051@ecc.ac.jp>
2024-03-21 23:11:11 +0900

    projectsページのサンプル一つ完成

--- src/components/projects/Miraitouhyou/page.tsx
+++ src/components/projects/Miraitouhyou/page.tsx
@@ -1,8 +1,13 @@
+import Btn from "@/components/btn/btn";
 import Image from "next/image";
 import Link from "next/link";
 
 
 export default function Miraitouhyou() {
+
+    const handleClick = () => {
+        window.open('/企画書.pdf');
+    }
     return(
         <>
             <section className="projectsTitle">
@@ -24,8 +29,41 @@ export default function Miraitouhyou() {
                 </div>
             </section>
             <Link href="https://mirai-touhyou.vercel.app/">https://mirai-touhyou.vercel.app/</Link>
-            <section>
-
+            <section className="projectsStory">
+                <h3>ストーリー</h3>
+                <p>
+                    1年後期の作品でECC EXPOに出展することを目標に考えてこの作品を制作しました。<br />
+                    ECC EXPOでは、社会問題を取り扱っている作品が選ばれやすいと聞いたので、社会問題を題材にしたサービスを作成した背景があります。
+                </p>
+                <h4>反省</h4>
+                <p style={{margin:'4px 0'}}>AIに頼りすぎた部分があるなと思ったことです。</p>
+                <p>
+                    あるエラーが出た時、どのようなようなエラーなのかを軽くしか調べずにAIに丸投げした結果その時はエラーが解消したんですが、<br />
+                    ビルドした際にAIに丸投げした部分がエラーが出て修正するのにすごく時間が掛かったことです。<br />
+                    次からは、程良くAIを取り入れていきたい。
+                </p>
+                <h4>成長したこと</h4>
+                <p style={{margin:'4px 0'}}>ミライ投票が完成に近ずく度に自分が成長しているなと実感できたことです。</p>
+                <p>
+                    完成が近づいた時に最初の方にコーディングしたところを編集する機会があった時に前に書いたコードを見た瞬間に、こっちのコードの方がいいと考え変更したように4ヶ月という短い時間でも最初の方よりは成長しているのかと実感しました。
+                </p>
+                <div>
+                    <Image src="/miraiConcept.svg" alt="ミライ投票コンセプトシート" width={830} height={587} />
+                </div>
+                <div>
+                    <button onClick={handleClick}>proposal</button>
+                </div>
+            </section>
+            <section className="projectsDesign">
+                <h3>デザイン</h3>
+                <p>
+                    ミライ投票のデザインは、シンプルでわかりやすいデザインを心がけました。<br />
+                    また、ユーザーがどのような操作をするのかを考えて、ボタンの配置や色などを工夫しました。
+                </p>
+                <div>
+                    <Image src="/miraiLogo.svg" alt="ミライ投票ロゴ" width={257} height={252} />
+                    <Image src="/miraiPoster.svg" alt="ミライ投票ポスター" width={385} height={545} />
+                </div>
             </section>
         </>
     )
